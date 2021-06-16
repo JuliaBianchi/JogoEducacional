9ddc6704969300c3b24db51a080350c997dfc4ba
Júlia <juliaspaderbianchi@gmail.com>
2021-06-16 10:55:05 -0300

    Versão 1 do jogo

--- jogo.py
+++ jogo.py
@@ -1,5 +1,6 @@
 import pygame
-
+import time
+import random
 
 #iniciando o pygame
 pygame.init()
@@ -10,22 +11,24 @@ altura = 720
 display = pygame.display.set_mode((largura,altura))
 fps = pygame.time.Clock()
 
-
 #características do jogo
 pygame.display.set_caption("O Mundo Mágico das Letras")
 icone = pygame.image.load("assets/mestre-dos-magos.jpg")
 pygame.display.set_icon(icone)
 
-#cores
+#variaveis
 preto = (0,0,0)
 branco = (255,255,255)
-
-#configurações do fundo
 fundo = pygame.image.load("assets/saladeaula1.jpg")
-
-#config - ator
 ator = pygame.image.load("assets/ator.png")
+fala = pygame.image.load("assets/fala.png")
 
+#palavras
+fonte = pygame.font.SysFont("arial", 50, True, False)
+#Pegar a posição do mouse
+x,y = pygame.mouse.get_pos()
+print(x)
+print(y)
 
 while True:
     #início - verificação de interação com o usuário
@@ -34,9 +37,16 @@ while True:
             pygame.quit()
             quit()
     #fim - verificação de interação com o usuário
-
+    
+    #config da tela
     display.blit(fundo, (0,0)) #inserir imagem na tela
-    display.blit(ator, (400,300))
+    display.blit(ator, (430,150))
+    display.blit(fala, (550,92))
+    palavras = ('Gat_, Ab_lha, Papaga_o, C_chorro, Tartar_ga')
+    texto = fonte.render(palavras, True, (255,255,255))
+    display.blit(texto,(719,255))
+
+
     pygame.display.update() 
     fps.tick(60)
 
