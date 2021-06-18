0eed17b126fef380f57510c3d043347ea5253dcf
Júlia <juliaspaderbianchi@gmail.com>
2021-06-17 23:09:36 -0300

    Física e colisão do balões queridos

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+from pygame.locals import *
 
 #iniciando o pygame
 pygame.init()
@@ -16,35 +17,83 @@ pygame.display.set_caption("O Mundo Mágico das Letras")
 icone = pygame.image.load("assets/mestre-dos-magos.jpg")
 pygame.display.set_icon(icone)
 
-#variaveis
-preto = (0,0,0)
-branco = (255,255,255)
-fundo = pygame.image.load("assets/saladeaula1.jpg")
-ator = pygame.image.load("assets/ator.png")
-fala = pygame.image.load("assets/fala.png")
-vogalA =  pygame.image.load("assets/balaoA.png")
-vogalE =  pygame.image.load("assets/balaoE.png")
-vogalI =  pygame.image.load("assets/balaoI.png")
-vogalO =  pygame.image.load("assets/balaoO.png")
-vogalU =  pygame.image.load("assets/balaoU.png")
+def jogo():
+    fundo = pygame.image.load("assets/saladeaula1.jpg")
+    ator = pygame.image.load("assets/ator.png")
+    mensagem = pygame.image.load("assets/fala.png")
 
-#Pegar a posição do mouse
-x,y = pygame.mouse.get_pos()
-print(x)
-print(y)
+    pontos = 0
 
-while True:
-    #início - verificação de interação com o usuário
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-    #fim - verificação de interação com o usuário
-    
-    #config da tela
-    display.blit(fundo, (0,0)) #inserir imagem na tela
-    display.blit(ator, (200,150))
-    display.blit(fala, (350,50))
-    pygame.display.update() 
-    fps.tick(60)
+    velocidade = 3
+    posicaoYbalao = 600
+    posicaoXbalao = largura * 0.45
+    larguraBalao = 90
+    alturaBalao = 212
+    listaPosicao = [random.randrange(50),random.randrange(55,200),random.randrange(300,400),random.randrange(600,700),random.randrange(800,900),random.randrange(1000,1110),random.randrange(100,260)]
+    listaVelocidade = [2,1,2,1,2,1,2]
+    listaYbaloes = [720,720,720,720,720,720,720]
+
+    #imagens - Balões
+    vogalA =  pygame.image.load("assets/a.png")
+    vogalE =  pygame.image.load("assets/e.png")
+    vogalI =  pygame.image.load("assets/i.png")
+    vogalO =  pygame.image.load("assets/o.png")
+    vogalU =  pygame.image.load("assets/u.png")
+    balaoLaranja = pygame.image.load("assets/balaolaranja.png")
+    balaoVerde = pygame.image.load("assets/balaoverde.png")
+
+
+    #Pegar a posição do mouse
+
+
+    while True:
+        #início - verificação de interação com o usuário
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit() 
+            if evento.type == MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                for i in range(0,7):
+                    if x >= listaPosicao[i] and x <= listaPosicao[i] +80:
+                        if y >= listaYbaloes[i] and y <= listaYbaloes[i]+100:
+                            listaYbaloes[i] = 720
+                if x >= posicaoXbalao and x <= posicaoXbalao +80:
+                    if y >= posicaoYbalao and y <= posicaoYbalao+100:
+                        posicaoYbalao = 720
+        #fim - verificação de interação com o usuário
+
+        #config da tela
+        display.blit(fundo, (0,0)) #inserir imagem na tela
+        display.blit(ator, (200,150))
+        display.blit(mensagem, (350,50))
+        #Balões subindo
+
+        listaYbaloes[0] = listaYbaloes[0] - listaVelocidade[0]
+        listaYbaloes[1] = listaYbaloes[1] - listaVelocidade[1]
+        listaYbaloes[2] = listaYbaloes[2] - listaVelocidade[2]
+        listaYbaloes[3] = listaYbaloes[3] - listaVelocidade[3]
+        listaYbaloes[4] = listaYbaloes[4] - listaVelocidade[4]
+        listaYbaloes[5] = listaYbaloes[5] - listaVelocidade[5]
+        listaYbaloes[6] = listaYbaloes[6] - listaVelocidade[6]
+        posicaoYbalao = posicaoYbalao - listaVelocidade[3]
+
+        if posicaoYbalao <= -200:
+            posicaoYbalao = 600
+            posicaoXbalao = random.randrange(0, largura-50)
+        for i in range(0,7):
+            if listaYbaloes [i] <= -200:
+                listaYbaloes[i] = 600
+                listaPosicao[i] = random.randrange(0, largura)
+                
+        display.blit(vogalA, (posicaoXbalao, posicaoYbalao))
+        display.blit(vogalE, (listaPosicao[0], listaYbaloes[0]))
+        display.blit(vogalI, (listaPosicao[1], listaYbaloes[1]))
+        display.blit(balaoLaranja, (listaPosicao[2], listaYbaloes[2]))
+        display.blit(vogalU, (listaPosicao[3], listaYbaloes[3]))
+        display.blit(vogalO, (listaPosicao[4], listaYbaloes[4]))
+        display.blit(balaoVerde, (listaPosicao[5], listaYbaloes[5]))
+        pygame.display.update() 
+        fps.tick(60)
+jogo()
 
