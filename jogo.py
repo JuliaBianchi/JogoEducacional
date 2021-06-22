ada4aef7f4ed60c875925ea9a6b00116267677dd
Júlia <juliaspaderbianchi@gmail.com>
2021-06-22 20:52:55 -0300

    Adicionanei sons e pontos

--- jogo.py
+++ jogo.py
@@ -17,13 +17,25 @@ pygame.display.set_caption("O Mundo Mágico das Letras")
 icone = pygame.image.load("assets/mestre-dos-magos.jpg")
 pygame.display.set_icon(icone)
 
+#cores
+preto = (0,0,0)
+branco = (255,255,255)
+
+#Placar de pontos
+
+def escrevendoPlacar(pontos):
+    font = pygame.font.SysFont(None, 45)
+    texto = font.render("Pontos: "+str(pontos), True, branco)
+    display.blit(texto, (800, 200))
+
 def jogo():
+    pygame.mixer.music.load("assets/somdefundo.mp3")
+    pygame.mixer.music.play(-1)
     fundo = pygame.image.load("assets/saladeaula1.jpg")
     ator = pygame.image.load("assets/ator.png")
     mensagem = pygame.image.load("assets/fala.png")
 
     pontos = 0
-
     velocidade = 3
     posicaoYbalao = 600
     posicaoXbalao = largura * 0.45
@@ -32,6 +44,7 @@ def jogo():
     listaPosicao = [random.randrange(50),random.randrange(55,200),random.randrange(300,400),random.randrange(600,700),random.randrange(800,900),random.randrange(1000,1110),random.randrange(100,260)]
     listaVelocidade = [2,1,2,1,2,1,2]
     listaYbaloes = [720,720,720,720,720,720,720]
+    estouroSound = pygame.mixer.Sound("assets/estouro.wav")
 
     #imagens - Balões
     vogalA =  pygame.image.load("assets/a.png")
@@ -41,10 +54,6 @@ def jogo():
     vogalU =  pygame.image.load("assets/u.png")
     balaoLaranja = pygame.image.load("assets/balaolaranja.png")
     balaoVerde = pygame.image.load("assets/balaoverde.png")
-
-
-    #Pegar a posição do mouse
-
 
     while True:
         #início - verificação de interação com o usuário
@@ -58,17 +67,22 @@ def jogo():
                     if x >= listaPosicao[i] and x <= listaPosicao[i] +80:
                         if y >= listaYbaloes[i] and y <= listaYbaloes[i]+100:
                             listaYbaloes[i] = 720
+                            pygame.mixer.Sound.play(estouroSound)
+                            pontos = pontos + 1
                 if x >= posicaoXbalao and x <= posicaoXbalao +80:
                     if y >= posicaoYbalao and y <= posicaoYbalao+100:
                         posicaoYbalao = 720
+                        pygame.mixer.Sound.play(estouroSound)
+                        pontos = pontos + 1
+                        
         #fim - verificação de interação com o usuário
 
         #config da tela
         display.blit(fundo, (0,0)) #inserir imagem na tela
         display.blit(ator, (200,150))
         display.blit(mensagem, (350,50))
-        #Balões subindo
 
+        #Balões subindo
         listaYbaloes[0] = listaYbaloes[0] - listaVelocidade[0]
         listaYbaloes[1] = listaYbaloes[1] - listaVelocidade[1]
         listaYbaloes[2] = listaYbaloes[2] - listaVelocidade[2]
@@ -93,7 +107,9 @@ def jogo():
         display.blit(vogalU, (listaPosicao[3], listaYbaloes[3]))
         display.blit(vogalO, (listaPosicao[4], listaYbaloes[4]))
         display.blit(balaoVerde, (listaPosicao[5], listaYbaloes[5]))
+        escrevendoPlacar(pontos)
         pygame.display.update() 
         fps.tick(60)
+
 jogo()
 
