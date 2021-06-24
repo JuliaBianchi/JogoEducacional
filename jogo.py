4942390f9eeae404cef52a5d13eec2639df5f7bc
Júlia <juliaspaderbianchi@gmail.com>
2021-06-23 21:35:41 -0300

    Ajustes finais do jogo

--- jogo.py
+++ jogo.py
@@ -24,18 +24,40 @@ pygame.display.set_icon(icone)
 #cores
 preto = (0,0,0)
 branco = (255,255,255)
+erros = 0
+
+#pegas a posição do mouse
+x,y = pygame.mouse.get_pos()
+print(x)
+print(y)
 
 #Placar de pontos
 def escrevendoPlacar(pontos):
     font = pygame.font.SysFont(None, 45)
     texto = font.render("Pontos: "+str(pontos), True, branco)
+    texto2 = font.render("Erros: "+str(erros), True, branco)
     display.blit(texto, (800, 200))
+    display.blit(texto2, (800, 250))
+
+def gameOver():
+    if erros == 5:
+        font = pygame.font.SysFont(None, 45)
+        texto = font.render("Você Perdeu :( "+str(erros)+" erros! ", True, branco)
+        display.blit(texto, (800, 300))
+        pygame.display.update() 
+        time.sleep(3)
+        pygame.quit()
+        quit()
+
+
 def jogo():
+    global erros
     pygame.mixer.music.load("assets/somdefundo.mp3")
     pygame.mixer.music.play(-1)
     fundo = pygame.image.load("assets/saladeaula1.jpg")
     ator = pygame.image.load("assets/ator.png")
     mensagem = pygame.image.load("assets/fala.png")
+    logo = pygame.image.load("assets/cc.png")
     pontos = 0
     velocidade = 3
     posicaoYbalao = 600
@@ -65,22 +87,34 @@ def jogo():
             #Pegar a posição do Balão
             if evento.type == MOUSEBUTTONDOWN:
                 x,y = pygame.mouse.get_pos()
-                for i in range(0,7):
+                for i in range(0,4):
                     if x >= listaPosicao[i] and x <= listaPosicao[i] +80:
                         if y >= listaYbaloes[i] and y <= listaYbaloes[i]+100:
                             listaYbaloes[i] = 720
                             pygame.mixer.Sound.play(estouroSound)
-                            pontos = pontos + 1
+                            pontos += 1
+                for i in range(4,6):
+                    if x >= listaPosicao[i] and x <= listaPosicao[i] +80:
+                        if y >= listaYbaloes[i] and y <= listaYbaloes[i]+100:
+                            listaYbaloes[i] = 720
+                            erros = erros + 1
+
                 if x >= posicaoXbalao and x <= posicaoXbalao +80:
                     if y >= posicaoYbalao and y <= posicaoYbalao+100:
                         posicaoYbalao = 720
                         pygame.mixer.Sound.play(estouroSound)
-                        pontos = pontos + 1
+                        pontos += 1
         #fim - verificação de interação com o usuário
-        #config da tela
+
         display.blit(fundo, (0,0)) #inserir imagem na tela
         display.blit(ator, (200,150))
-        display.blit(mensagem, (350,50))
+        display.blit(mensagem, (390,50))
+        display.blit(logo,(937,420))
+
+        escrevendoPlacar(pontos)
+        gameOver()
+
+
 
         #Balões subindo
         listaYbaloes[0] = listaYbaloes[0] - listaVelocidade[0]
@@ -91,6 +125,7 @@ def jogo():
         listaYbaloes[5] = listaYbaloes[5] - listaVelocidade[5]
         listaYbaloes[6] = listaYbaloes[6] - listaVelocidade[6]
         posicaoYbalao = posicaoYbalao - listaVelocidade[3]
+
         if posicaoYbalao <= -200:
             posicaoYbalao = 600
             posicaoXbalao = random.randrange(0, largura-50)
@@ -98,17 +133,14 @@ def jogo():
             if listaYbaloes [i] <= -200:
                 listaYbaloes[i] = 600
                 listaPosicao[i] = random.randrange(0, largura)   
+
         display.blit(vogalA, (posicaoXbalao, posicaoYbalao))
         display.blit(vogalE, (listaPosicao[0], listaYbaloes[0]))
         display.blit(vogalI, (listaPosicao[1], listaYbaloes[1]))
-        display.blit(balaoLaranja, (listaPosicao[2], listaYbaloes[2]))
-        display.blit(vogalU, (listaPosicao[3], listaYbaloes[3]))
-        display.blit(vogalO, (listaPosicao[4], listaYbaloes[4]))
-        display.blit(balaoVerde, (listaPosicao[5], listaYbaloes[5]))
-        escrevendoPlacar(pontos)
+        display.blit(vogalU, (listaPosicao[2], listaYbaloes[2]))
+        display.blit(vogalO, (listaPosicao[3], listaYbaloes[3]))
+        display.blit(balaoVerde, (listaPosicao[4], listaYbaloes[4]))
+        display.blit(balaoLaranja, (listaPosicao[5], listaYbaloes[5]))
         pygame.display.update() 
         fps.tick(60)
 jogo()
-
-
-
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import cx_Freeze
-executables = [cx_Freeze.Executable{
-    script="jogo.py", icon="assets/mestre-dos-magos.jpg"}]
+
+executables = [cx_Freeze.Executable (
+    script="jogo.py", icon="assets/mestre-dos-magos.ico")]
+
 cx_Freeze.setup(
-    nome = "O Mundo Mágico das Letras",
-    options ={
-        "build_exe":{
-            "packages":["pygame"],
+    name="O Mundo Mágico das Letras",
+    options={
+        "build_exe": {
+            "packages": ["pygame"],
             "include_files":["assets"]
         }},
-        executables=executables
+    executables=executables
 )
